2dc4bc3311cbc13c80cab4e26efb959cb8e86592
Tran Nhat Tien <43870130+nhattientran@users.noreply.github.com>
2026-04-05 21:22:22 +0700

    Add flake.nix for Ubuntu WSL dev environment

---
+++ nix/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Ubuntu WSL dev env with Home Manager + Neovim NvChad";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      homeConfigurations."tientn" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+}
